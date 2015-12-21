27d904936376628b819efd3ca6c3da17a40da41e
rcasetta <Richard@MacBook-Pro-8.local>
2015-12-22 00:46:24 +0100

    modif

--- code/traitement/accueil.php
+++ code/traitement/accueil.php
@@ -5,17 +5,17 @@ session_start();
 include_once('../classes/groupe.php');
 include_once('../classes/compte');
 
-$groupeManager = new GroupeManager();
+$groupeManager = new GroupeManager($bd);
 $groupes = $groupeManager->getList();
 echo json_encode($groupes);
 
-$compteManager = new CompteManager();
+$compteManager = new CompteManager($bd);
 $compte = $compteManager->get($_SESSION['id']);
 echo json_encode($compte);
 
 $groupesBug = [];
 
-$request = $bd->pquery('SELECT * FROM groupe g
+$request = $bd->query('SELECT * FROM groupe g
     JOIN constitution_groupe cp ON cp.IDGroupe = g.id
     JOIN objet o ON o.id = cp.idObjet
     WHERE o.etatBug = 1');
