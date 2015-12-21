33ed684ea9061fe81f5186b26b0a2eef5562af18
rcasetta <Richard@MacBook-Pro-8.local>
2015-12-22 00:38:18 +0100

    ajout bug groupe

--- code/traitement/accueil.php
+++ code/traitement/accueil.php
@@ -13,4 +13,20 @@ $compteManager = new CompteManager();
 $compte = $compteManager->get($_SESSION['id']);
 echo json_encode($compte);
 
+$groupesBug = [];
+
+$request = $bd->pquery('SELECT * FROM groupe g
+    JOIN constitution_groupe cp ON cp.IDGroupe = g.id
+    JOIN objet o ON o.id = cp.idObjet
+    WHERE o.etatBug = 1');
+if ($request)
+{
+    while ($donnees = $request->fetch())
+    {
+        $groupesBug[] = $donnees;
+    }
+}
+$request->closeCursor();
+echo json_encode($groupesBug);
+
 ?>
