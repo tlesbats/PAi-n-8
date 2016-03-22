0a25c4b50046065754f91dd5f81206d30601cff2
rcasetta <richard.casetta@gmail.com>
2016-03-22 14:44:31 +0100

    Modif afficher_liste_interrupteur

--- code/traitement/afficher_liste_interrupteur.php
+++ code/traitement/afficher_liste_interrupteur.php
@@ -1,8 +1,8 @@
 <?php
 
 $request = $db->prepare('SELECT iv.id, iv.nom, iv.etat FROM iv
-	JOIN constitution_groupe cp ON cp.IDObjet = iv.id
-	JOIN groupe g ON g.id = cp.IDGroupe WHERE g.id = :id');
+	JOIN pilotage_iv_charge pic ON pic.IDIv = iv.id
+	JOIN groupe g ON g.id = pic.IDGroupe WHERE g.id = :id');
 
 $request->bindParam(':id', $_POST['salle'], PDO::PARAM_STR);
 $request->execute();
