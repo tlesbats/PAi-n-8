db65dd49754ab7ab13f36acbcefe38a095f15bf4
rcasetta <richard.casetta@gmail.com>
2016-03-22 15:04:07 +0100

    Modif modifier_etat_interrupteur

--- code/traitement/modifier_etat_interrupteur.php
+++ code/traitement/modifier_etat_interrupteur.php
@@ -1,6 +1,11 @@
 <?php
 
-$request = $bd->query('SELECT c.id, c.etatCommande, c.pilotable FROM charge c
+$request = $bd->query('SELECT pilotable FROM iv WHERE id = ' . $_POST['id']);
+$donnee = $request->fetch();
+$etatIv = $donnee['pilotable'];
+$request->closeCursor();
+
+$request = $bd->query('SELECT c.id, c.pilotable FROM charge c
 	JOIN pilotage_iv_charge pic ON pic.C_IDObjet = c.id WHERE pic.id = ' . $_POST['id']);
 
 $ids = [];
@@ -8,8 +13,10 @@ while ($donnee = $request->fetch())
 {
 	if ($donnee['pilotable'])
 	{
-		$request = $bd->query('UPDATE charge SET etatCommande = ' . (1 - $donnee['etatCommande']) . ' WHERE id = ' . $donnee['id']);
+		$request = $bd->query('UPDATE charge SET etatCommande = ' . $etatIv . ' WHERE id = ' . $donnee['id']);
 		$ids[] = $donnee['id'];
 	}
 }
+$request->closeCursor();
+
 echo json_encode($ids);
