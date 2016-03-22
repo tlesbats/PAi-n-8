71d03bb522997cd6b59169781df9a9b4a21d5d6b
rcasetta <richard.casetta@gmail.com>
2016-03-22 15:44:22 +0100

    modif mineure

--- code/traitement/modifier_etat_interrupteur.php
+++ code/traitement/modifier_etat_interrupteur.php
@@ -10,17 +10,17 @@ $request->closeCursor();
 
 $request = $bdd->query('SELECT c.id, c.pilotable FROM charge c
 	JOIN constitution_groupe cp ON cp.IDObjet = c.id
-	JOIN pilotage_iv_charge pic ON pic.IDGroupe_Affichage = cp.IDGroupe WHERE pic.id = ' . $_POST['id']);
+	JOIN pilotage_iv_charge pic ON pic.IDGroupe = cp.IDGroupe WHERE pic.id = ' . $_POST['id']);
 
 $ids = [];
 while ($donnee = $request->fetch())
 {
 	if ($donnee['pilotable'])
 	{
-		$request = $bdd->query('UPDATE charge SET etatCommande = ' . $nouvelEtatIv . ' WHERE id = ' . $donnee['id']);
+		$request2 = $bdd->query('UPDATE objet SET etatEffectif = ' . $nouvelEtatIv . ' WHERE id = ' . $donnee['id']);
 		$ids[] = $donnee['id'];
 	}
 }
-$request->closeCursor();
+$request2->closeCursor();
 
 echo json_encode($ids);
